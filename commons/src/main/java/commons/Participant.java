43cc9a65427c233cba72988e2ae86068c6958d9a
Philip Lekkerkerker <p.s.lekkerkerker@student.tudelft.nl>
2024-02-20 19:47:08 +0100

    Fixing Equals methode

--- commons/src/main/java/commons/Participant.java
+++ commons/src/main/java/commons/Participant.java
@@ -1,13 +1,13 @@
 package commons;
 
-
-import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
+import java.util.Objects;
 
 @Entity
 public class Participant {
@@ -42,15 +42,21 @@ public class Participant {
         this.bic = bic;
     }
 
+
+
     /**
      * An equals methode for to compare 2 participants
-     * @param obj The object to compare to the participant
+     * @param o The object to compare to the participant
      * @return returns if the other participant is the same as the one this function is called on
      */
     @Override
-    public boolean equals(Object obj) {
-        return EqualsBuilder.reflectionEquals(this, obj);
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Participant that = (Participant) o;
+        return Objects.equals(name, that.name) && Objects.equals(email, that.email) && Objects.equals(iban, that.iban) && Objects.equals(bic, that.bic);
     }
+
 
     /**
      * Function to hash a participant
