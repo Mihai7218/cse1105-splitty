7a8338db993eff28e50e386f7cea877658eba694
Philip Lekkerkerker <p.s.lekkerkerker@student.tudelft.nl>
2024-02-20 18:03:14 +0100

    Added Test for the Participant.java in the ParticipantTest.java. All passed

---
+++ commons/src/test/java/commons/ParticipantTest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2021 Delft University of Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package commons;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class ParticipantTest {
+
+    @Test
+    public void ConstructorTest() {
+        var p = new Participant("Joe", "joe.doe@gmail.com", "NL12 1923 1237 8374 02", "ALSUENBG");
+        assertEquals("Joe", p.name);
+        assertEquals("joe.doe@gmail.com", p.email);
+        assertEquals("NL12 1923 1237 8374 02", p.iban);
+        assertEquals("ALSUENBG", p.bic);
+    }
+
+    @Test
+    public void ToStringTest() {
+        var actual = new Participant("Joe", "joe.doe@gmail.com", "NL12 1923 1237 8374 02", "ALSUENBG").toString();
+        var ewa = actual.substring(28);
+        assertTrue(actual.contains(Participant.class.getSimpleName()));
+        assertEquals("[\r\n" + "  bic=ALSUENBG\r\n" +
+                "  email=joe.doe@gmail.com\r\n" +
+                "  iban=NL12 1923 1237 8374 02\r\n" +
+                "  id=0\r\n" +
+                "  name=Joe\r\n" +
+                "]", actual.substring(28));
+    }
+
+    @Test
+    public void EqualsTest() {
+        var participant1 = new Participant("Joe", "joe.doe@gmail.com", "NL12 1923 1237 8374 02", "ALSUENBG");
+        var participant2 = new Participant("Joe", "joe.doe@gmail.com", "NL12 1923 1237 8374 02", "ALSUENBG");
+        assertEquals(participant1, participant2);
+    }
+    @Test
+    public void NotEqualsTest() {
+        var participant1 = new Participant("Joe", "joe.doe@gmail.com", "NL12 1923 1237 8374 02", "ALSUENBG");
+        var participant2 = new Participant("Jo", "joe.doe@gmail.com", "NL12 1923 1237 8374 02", "ALSUENBG");
+        assertNotEquals(participant1, participant2);
+    }
+
+    @Test
+    public void EqualHashCodeTest() {
+        var participant1 = new Participant("Joe", "joe.doe@gmail.com", "NL12 1923 1237 8374 02", "ALSUENBG");
+        var participant2 = new Participant("Joe", "joe.doe@gmail.com", "NL12 1923 1237 8374 02", "ALSUENBG");
+        assertEquals(participant2.hashCode(), participant1.hashCode());
+    }
+    @Test
+    public void NotEqualHashCodeTest() {
+        var participant1 = new Participant("Joe", "joe.doe@gmail.com", "NL12 1923 1237 8374 02", "ALSUENBG");
+        var participant2 = new Participant("Jo", "joe.doe@gmail.com", "NL12 1923 1237 8374 02", "ALSUENBG");
+        assertNotEquals(participant2.hashCode(), participant1.hashCode());
+    }
+}
