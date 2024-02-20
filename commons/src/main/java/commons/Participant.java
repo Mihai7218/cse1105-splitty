d17f88b6f9c5a0bb22cf6cd1190f99aaaeb96e25
Philip Lekkerkerker <p.s.lekkerkerker@student.tudelft.nl>
2024-02-20 17:33:36 +0100

    Added basic fields to the Participant.java

--- commons/src/main/java/commons/Participant.java
+++ commons/src/main/java/commons/Participant.java
@@ -1,4 +1,51 @@
 package commons;
 
+import static org.apache.commons.lang3.builder.ToStringStyle.MULTI_LINE_STYLE;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Participant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long id;
+
+    public String name;
+    public String email;
+    public String IBAN;
+    public String BIC;
+
+    @SuppressWarnings("unused")
+    private Participant() {
+    }
+
+    public Participant(String name, String email, String IBAN, String BIC) {
+        this.name = name;
+        this.email = email;
+        this.IBAN = IBAN;
+        this.BIC = BIC;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, MULTI_LINE_STYLE);
+    }
 }
