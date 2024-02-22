baedf2a31a8a3b1665529972653ce15c732b58ba
Zico Krassenburg <z.i.k.krassenburg@student.tudelft.nl>
2024-02-22 14:55:53 +0100

    Improved some names and initialize the lists for the event as an empty list now

--- commons/src/main/java/commons/Event.java
+++ commons/src/main/java/commons/Event.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
@@ -17,12 +18,12 @@ public class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
 
-    public int invitecode;
+    public String invitecode;
     public String title;
     // TO DO: specify the list to be a list of expenses
-    public List expenses;
-    public List<Participant> participants;
-    public List<Tag> tags;
+    public List expensesList;
+    public List<Participant> participantsList;
+    public List<Tag> tagsList;
     public Date creationdate;
     public Date lastactivity;
 
@@ -37,19 +38,15 @@ public class Event {
      * Constructor for a event
      * @param invitecode The invitecode of the event
      * @param title The title of the event
-     * @param expenses The expenses of the event
-     * @param participants The participants of the event
-     * @param tags The tags of the event
      * @param creationdate The creation date of the event
      * @param lastactivity The last activity of the event
      */
-    public Event(int invitecode, String title, List expenses, List<Participant> participants,
-                 List<Tag> tags, Date creationdate, Date lastactivity) {
+    public Event(String invitecode, String title, Date creationdate, Date lastactivity) {
         this.invitecode = invitecode;
         this.title = title;
-        this.expenses = expenses;
-        this.participants = participants;
-        this.tags = tags;
+        this.expensesList = new ArrayList<>();
+        this.participantsList = new ArrayList<>();
+        this.tagsList = new ArrayList<>();
         this.creationdate = creationdate;
         this.lastactivity = lastactivity;
     }
@@ -67,8 +64,8 @@ public class Event {
         Event that = (Event) o;
         return Objects.equals(invitecode, that.invitecode) &&
                 Objects.equals(title, that.title) &&
-                Objects.equals(expenses, that.participants) &&
-                Objects.equals(tags, that.tags) &&
+                Objects.equals(expensesList, that.participantsList) &&
+                Objects.equals(tagsList, that.tagsList) &&
                 Objects.equals(creationdate, that.creationdate) &&
                 Objects.equals(lastactivity, that.lastactivity);
     }
@@ -92,8 +89,8 @@ public class Event {
         return "Event{" +
                 "invitecode='" + invitecode + '\'' +
                 ", title='" + title + '\'' +
-                ", expenses='" + expenses + '\'' +
-                ", tags='" + tags + '\'' +
+                ", expenses='" + expensesList + '\'' +
+                ", tags='" + tagsList + '\'' +
                 ", creationdate='" + creationdate + '\'' +
                 ", lastactivity='" + lastactivity + '\'' +
                 '}';
