53fa573a17ab51063fec72e78c1a41ce27feee7b
Mihai Bratu <m.a.bratu@student.tudelft.nl>
2024-02-22 11:36:31 +0100

    Changed Expense to be more consistent with other entities

--- commons/src/main/java/commons/Expense.java
+++ commons/src/main/java/commons/Expense.java
@@ -1,25 +1,22 @@
 package commons;
 
 import jakarta.persistence.*;
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
-import org.apache.commons.lang3.builder.ToStringBuilder;
-
-import static org.apache.commons.lang3.builder.ToStringStyle.MULTI_LINE_STYLE;
+import java.util.Date;
+import java.util.Objects;
 
 @Entity
 public class Expense {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    public long id;
+    private long id;
     public double amount;
     public String currency;
-
-    @OneToOne(cascade = CascadeType.ALL)
-    public ExpenseTitleDescriptionDate titleDescriptionDate;
-    @ManyToMany (cascade = CascadeType.REMOVE)
-    public List<ParticipantPayment> split;
+    public String title;
+    public String description;
+    public Date date;
+    @OneToMany (cascade = CascadeType.REMOVE)
+    private List<ParticipantPayment> split;
     @ManyToOne
     public Tag tag;
     @ManyToOne
@@ -29,36 +26,66 @@ public class Expense {
      * No-arg constructor of Expense
      */
     @SuppressWarnings("unused")
-    public Expense() {
+    private Expense() {
     }
 
     /**
      * Constructor of an expense object
      * @param amount - Amount of the expense
      * @param currency - Currency of the expense
-     * @param etdd - Object storing the title, description and date of the expense
+     * @param title - Title of the expense
+     * @param description - Description of the expense
+     * @param date - Date of the expense
      * @param split - List of participants that need to pay and the amount owed
      * @param tag - Tag of the expense
      * @param payee - Participant that needs to be paid back
      */
-    public Expense(double amount, String currency, ExpenseTitleDescriptionDate etdd,
+    public Expense(double amount, String currency, String title, String description, Date date,
                    List<ParticipantPayment> split, Tag tag, Participant payee) {
         this.currency = currency;
         this.amount = amount;
-        this.titleDescriptionDate = etdd;
+        this.title = title;
+        this.description = description;
+        this.date = date;
         this.split = split;
         this.tag = tag;
         this.payee = payee;
     }
 
     /**
+     * Getter for the ID
+     * @return the ID of the object
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * Getter for the list of ParticipantPayments
+     * @return the list of ParticipantPayments
+     */
+    public List<ParticipantPayment> getSplit() {
+        return split;
+    }
+
+    /**
      * Equals method of an expense
-     * @param obj - Object to check equality with.
+     * @param o - Object to check equality with.
      * @return true if equal and false otherwise.
      */
     @Override
-    public boolean equals(Object obj) {
-        return EqualsBuilder.reflectionEquals(this, obj);
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Expense expense = (Expense) o;
+        return id == expense.id && Double.compare(amount, expense.amount) == 0
+                && Objects.equals(currency, expense.currency)
+                && Objects.equals(title, expense.title)
+                && Objects.equals(description, expense.description)
+                && Objects.equals(date, expense.date)
+                && Objects.equals(split, expense.split)
+                && Objects.equals(tag, expense.tag)
+                && Objects.equals(payee, expense.payee);
     }
 
     /**
@@ -67,7 +94,7 @@ public class Expense {
      */
     @Override
     public int hashCode() {
-        return HashCodeBuilder.reflectionHashCode(this);
+        return Objects.hash(id, amount, currency, title, description, date, split, tag, payee);
     }
 
     /**
@@ -76,6 +103,16 @@ public class Expense {
      */
     @Override
     public String toString() {
-        return ToStringBuilder.reflectionToString(this, MULTI_LINE_STYLE);
+        return "Expense{" +
+                "id=" + id +
+                ", amount=" + amount +
+                ", currency='" + currency + '\'' +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", date=" + date +
+                ", split=" + split +
+                ", tag=" + tag +
+                ", payee=" + payee +
+                '}';
     }
 }
