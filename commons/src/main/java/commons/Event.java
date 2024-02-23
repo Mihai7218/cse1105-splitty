6c164dab8b9ed5b38c70b5534b51a737cdd38a1d
Azra Ocak <a.ocak@student.tudelft.nl>
2024-02-23 20:08:46 +0100

    made some changes to the Event class, added setup for EventTest class

--- commons/src/main/java/commons/Event.java
+++ commons/src/main/java/commons/Event.java
@@ -13,18 +13,16 @@ import java.util.Objects;
 
 @Entity
 public class Event {
-
+    //TODO: change invite code into string
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-
-    public int invitecode;
-    public String title;
-    // TO DO: specify the list to be a list of expenses
-    public List expenses;
-    public List<Participant> participants;
-    public List<Tag> tags;
-    public Date creationdate;
-    public Date lastactivity;
+    private int inviteCode;
+    private String title;
+    private List<Expense> expensesList;
+    private List<Participant> participantsList;
+    private List<Tag> tagsList;
+    private Date creationDate;
+    private Date lastActivity;
 
     /**
      * Empty constructor for object mapper
@@ -34,26 +32,127 @@ public class Event {
     }
 
     /**
-     * Constructor for a event
-     * @param invitecode The invitecode of the event
+     * Constructor for an event
+     * @param inviteCode The inviteCode of the event
      * @param title The title of the event
-     * @param expenses The expenses of the event
-     * @param participants The participants of the event
-     * @param tags The tags of the event
-     * @param creationdate The creation date of the event
-     * @param lastactivity The last activity of the event
+     * @param expensesList The expenses of the event
+     * @param participantsList The participants of the event
+     * @param tagsList The tags of the event
+     * @param creationDate The creation date of the event
+     * @param lastActivity The last activity of the event
      */
-    public Event(int invitecode, String title, List expenses, List<Participant> participants,
-                 List<Tag> tags, Date creationdate, Date lastactivity) {
-        this.invitecode = invitecode;
+    public Event(int inviteCode, String title, List<Expense> expensesList, List<Participant> participantsList,
+                 List<Tag> tagsList, Date creationDate, Date lastActivity) {
+        this.inviteCode = inviteCode;
         this.title = title;
-        this.expenses = expenses;
-        this.participants = participants;
-        this.tags = tags;
-        this.creationdate = creationdate;
-        this.lastactivity = lastactivity;
+        this.expensesList = expensesList;
+        this.participantsList = participantsList;
+        this.tagsList = tagsList;
+        this.creationDate = creationDate;
+        this.lastActivity = lastActivity;
     }
 
+
+    /**
+     * @return returns the invite code of the event
+     */
+    public int getInviteCode() {
+        return inviteCode;
+    }
+
+    /**
+     * @param inviteCode sets the invite code of the event
+     */
+    public void setInviteCode(int inviteCode) {
+        this.inviteCode = inviteCode;
+    }
+
+    /**
+     * @return the title of the event
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title rename the event
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return the list of expenses of the event
+     */
+    public List<Expense> getExpensesList() {
+        return expensesList;
+    }
+
+    /**
+     * @param expensesList set the expenses list to a new list
+     *                     TODO: addExpense method
+     */
+    public void setExpensesList(List<Expense> expensesList) {
+        this.expensesList = expensesList;
+    }
+
+    /**
+     * @return the list of participants of the event
+     */
+    public List<Participant> getParticipantsList() {
+        return participantsList;
+    }
+
+    /**
+     * @param participantsList set the list of participants of the event
+     *                         TODO: addParticipant method
+     */
+    public void setParticipantsList(List<Participant> participantsList) {
+        this.participantsList = participantsList;
+    }
+
+    /**
+     * @return the tags of the event
+     */
+    public List<Tag> getTagsList() {
+        return tagsList;
+    }
+
+    /**
+     * @param tagsList change the list of tags of the event
+     *                 TODO: addTag method
+     */
+    public void setTagsList(List<Tag> tagsList) {
+        this.tagsList = tagsList;
+    }
+
+    /**
+     * @return the date the event was created
+     */
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    /**
+     * @param creationDate change/set the creation date of the event
+     */
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    /**
+     * @return the last time the event was managed
+     */
+    public Date getLastActivity() {
+        return lastActivity;
+    }
+
+    /**
+     * @param lastActivity change/set the date the event was last managed
+     */
+    public void setLastActivity(Date lastActivity) {
+        this.lastActivity = lastActivity;
+    }
 
     /**
      * An equals methode for to compare 2 event
@@ -65,18 +164,18 @@ public class Event {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Event that = (Event) o;
-        return Objects.equals(invitecode, that.invitecode) &&
+        return Objects.equals(inviteCode, that.inviteCode) &&
                 Objects.equals(title, that.title) &&
-                Objects.equals(expenses, that.participants) &&
-                Objects.equals(tags, that.tags) &&
-                Objects.equals(creationdate, that.creationdate) &&
-                Objects.equals(lastactivity, that.lastactivity);
+                Objects.equals(expensesList, that.expensesList) &&
+                Objects.equals(participantsList, that.participantsList) &&
+                Objects.equals(tagsList, that.tagsList) &&
+                Objects.equals(creationDate, that.creationDate) &&
+                Objects.equals(lastActivity, that.lastActivity);
     }
 
-
     /**
-     * Function to hash a Event
-     * @return returns a hashcode of a Event
+     * Function to hash an Event
+     * @return returns a hashcode of an Event
      */
     @Override
     public int hashCode() {
@@ -84,18 +183,19 @@ public class Event {
     }
 
     /**
-     * A toString methode to display a participant in a human-readable format
-     * @return returns a human-readable format of a event
+     * A toString method to display an event in a human-readable format
+     * @return returns a human-readable format of an event
      */
     @Override
     public String toString() {
         return "Event{" +
-                "invitecode='" + invitecode + '\'' +
+                "inviteCode='" + inviteCode + '\'' +
                 ", title='" + title + '\'' +
-                ", expenses='" + expenses + '\'' +
-                ", tags='" + tags + '\'' +
-                ", creationdate='" + creationdate + '\'' +
-                ", lastactivity='" + lastactivity + '\'' +
+                ", expenses='" + expensesList.toString() + '\'' +
+                ", participants='" + participantsList.toString() + '\'' +
+                ", tags='" + tagsList.toString() + '\'' +
+                ", creationDate='" + creationDate + '\'' +
+                ", lastActivity='" + lastActivity + '\'' +
                 '}';
     }
 }
--- commons/src/test/java/commons/EventTest.java
+++ commons/src/test/java/commons/EventTest.java
@@ -1,9 +1,104 @@
 package commons;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EventTest {
 
+    private Date creationDate;
+    private Date lastActivity;
+    private Event sameEvent;
+    private Event differentEvent;
+    private Event event;
+
+    @BeforeEach
+    void setUp() {
+        List<Expense> expensesList = new ArrayList<>();
+        List<Participant> participantsList = new ArrayList<>();
+        List<Tag> tagsList = new ArrayList<>();
+        creationDate = new Date();
+        lastActivity = new Date();
+
+        event = new Event(1, "Test Event", expensesList, participantsList, tagsList, creationDate, lastActivity);
+        sameEvent = new Event(1, "Test Event", expensesList, participantsList, tagsList, creationDate, lastActivity);
+        differentEvent = new Event(2, "Different Event", new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), creationDate, lastActivity);
+    }
+
+    @Test
+    void getInviteCode() {
+    }
+
+    @Test
+    void setInviteCode() {
+    }
+
+    @Test
+    void getTitle() {
+    }
+
+    @Test
+    void setTitle() {
+    }
+
+    @Test
+    void getExpensesList() {
+    }
+
+    @Test
+    void setExpensesList() {
+    }
+
+    @Test
+    void getParticipantsList() {
+    }
+
+    @Test
+    void setParticipantsList() {
+    }
+
+    @Test
+    void getTagsList() {
+    }
+
+    @Test
+    void setTagsList() {
+    }
+
+    @Test
+    void getCreationDate() {
+    }
+
+    @Test
+    void setCreationDate() {
+    }
+
+    @Test
+    void getLastActivity() {
+    }
+
+    @Test
+    void setLastActivity() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void testToString() {
+    }
 }
