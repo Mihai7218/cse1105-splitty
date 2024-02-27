3ab5372303d2eb0d6fb704b41781bc323a0ee350
Mihai Bratu <m.a.bratu@student.tudelft.nl>
2024-02-27 22:48:08 +0100

    Fix missing javadoc in TestConfig

--- client/src/test/java/client/scenes/TestConfig.java
+++ client/src/test/java/client/scenes/TestConfig.java
@@ -7,11 +7,21 @@ import java.util.HashMap;
 public class TestConfig implements ConfigInterface {
     public HashMap<String, String> config = new HashMap<>();
 
+    /**
+     * Getter for a property.
+     * @param key - key of the property
+     * @return - value of the property
+     */
     @Override
     public String getProperty(String key) {
         return config.get(key);
     }
 
+    /**
+     * Setter for a property.
+     * @param key   - key of the property
+     * @param value - value of the property
+     */
     @Override
     public void setProperty(String key, String value) {
         config.put(key, value);
