aedd45df98805cf3611ba4fd5f35e50c5eac1d55
Stella Marlene Schultz <S.M.Schultz@student.tudelft.nl>
2024-02-28 17:34:30 +0100

    Added javadoc and updated put/post methods

--- server/src/main/java/server/api/ParticipantController.java
+++ server/src/main/java/server/api/ParticipantController.java
@@ -4,7 +4,6 @@ import commons.Participant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import server.database.ParticipantRepository;
 
 import java.util.List;
 
@@ -19,25 +18,45 @@ public class ParticipantController {
      */
     private final ParticipantService participantService;
 
+    /**
+     * Participant Controller Constructor
+     * @param participantService participant service used for this controller
+     */
     @Autowired
     public ParticipantController(ParticipantService participantService){
         this.participantService = participantService;
     }
 
+    /**
+     * Get method to return list of participants associated with an event
+     * @param eventId the id of the event to get participants from
+     * @return the list of participants if the event is found
+     */
     @GetMapping(path = { "/{eventId}/participants", "/{eventId}/participants/"})
-    public List<Participant> getParticipants(
+    public ResponseEntity<List<Participant>> getParticipants(
             @PathVariable("eventId") long eventId) {
         return participantService.getAllParticipants(eventId);
     }
 
+    /**
+     * Get method to return a specific participant associated with an event
+     * @param eventId id of the event being looked at
+     * @param id id of the participant in the event
+     * @return the requested participant if found
+     */
     @GetMapping(path = {"/{eventId}/participants/{id}", "/{eventId}/participants/{id}/"})
-    public Participant getParticipant(
+    public ResponseEntity<Participant> getParticipant(
             @PathVariable("eventId") long eventId,
             @PathVariable("id") long id){
         return participantService.getParticipant(eventId, id);
     }
 
-
+    /**
+     * Post method to add a new participant to an event
+     * @param eventId id of the event that the participant is added to
+     * @param participant the participant to be added to the event
+     * @return the participant if successfully made or badrequest
+     */
     @PostMapping(path = {"/{eventId}/participants", "/{eventId}/participants/"})
     public ResponseEntity<Participant> addParticipant(
             @PathVariable("eventId") long eventId,
@@ -45,6 +64,16 @@ public class ParticipantController {
         return participantService.addParticipant(eventId, participant);
     }
 
+    /**
+     * Put method to update a participant in an event
+     * @param eventId id of the event the participant is in
+     * @param id id of participant to update
+     * @param name value to change name to
+     * @param email value to change email to
+     * @param iban value to change iban to
+     * @param bic value to change bic to
+     * @return the participant after it is updated or badrequest
+     */
     @PutMapping(path = {"/{eventId}/participants/{id}"})
     public ResponseEntity<Participant> updateParticipant(
             @PathVariable("eventId") long eventId,
@@ -56,15 +85,17 @@ public class ParticipantController {
         return participantService.updateParticipant(eventId, id, name, email, iban, bic);
     }
 
+    /**
+     * Delete method to remove a participant from an event
+     * @param eventId id of the event the participant was in
+     * @param id id of the participant to be removed
+     */
     @DeleteMapping(path = {"/{eventId}/participants/{id}"})
     public void deleteParticipant(
             @PathVariable("eventId") long eventId,
             @PathVariable("id") long id){
         participantService.deleteParticipant(eventId, id);
     }
-
-
-
 
 
 }
--- server/src/main/java/server/api/ParticipantService.java
+++ server/src/main/java/server/api/ParticipantService.java
@@ -1,8 +1,6 @@
 package server.api;
 
-import commons.Event;
 import commons.Participant;
-import jakarta.servlet.http.Part;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,9 +8,9 @@ import org.springframework.stereotype.Service;
 import server.database.EventRepository;
 import server.database.ParticipantRepository;
 
-import java.beans.Transient;
 import java.util.List;
 import java.util.Objects;
+import java.util.regex.Pattern;
 
 @Service
 public class ParticipantService {
@@ -20,6 +18,11 @@ public class ParticipantService {
     private final EventRepository eventRepository;
 
 
+    /**
+     * ParticipantService Constructor
+     * @param participantRepository the participant repository to store participants
+     * @param eventRepository the event repository to retrieve events from
+     */
     @Autowired
     public ParticipantService(ParticipantRepository participantRepository,
                               EventRepository eventRepository){
@@ -27,60 +30,115 @@ public class ParticipantService {
         this.eventRepository = eventRepository;
     }
 
-    public List<Participant> getAllParticipants(long eventId) {
+    /**
+     * Get method that checks if the id is valid and retrieves the list of participants
+     * @param eventId id of the event to get participants from
+     * @return list of participants if successfully retrieved
+     */
+    public ResponseEntity<List<Participant>> getAllParticipants(long eventId) {
         if(eventId < 0 || !eventRepository.existsById(eventId)){
-            throw new IllegalStateException("The event with id " + eventId + " does not exist.");
+            return ResponseEntity.badRequest().build();
+        }else if(eventRepository.findById(eventId).isPresent()) {
+            return ResponseEntity.ok(
+                    eventRepository.findById(eventId).get().getParticipantsList());
+        }else {
+            return ResponseEntity.badRequest().build();
         }
-        // TODO
-        return eventRepository.findById(eventId).get().getParticipantsList();
     }
 
-    public Participant getParticipant(long eventId, long id) {
-        List<Participant> participants = getAllParticipants(eventId);
-        if(id < 0 || !participantRepository.existsById(id)){
-            throw new IllegalStateException("The participant with id " + id + " does not exist.");
-
+    /**
+     * Get method that validates the event and participant id and retrieves the participant
+     * @param eventId the id of the event to get the participant from
+     * @param id of the participant to retrieve
+     * @return participant if succesfully retrieved
+     */
+    public ResponseEntity<Participant> getParticipant(long eventId, long id) {
+        List<Participant> participants = getAllParticipants(eventId).getBody();
+        if(id < 0 || !participantRepository.existsById(id) || participants==null){
+            return ResponseEntity.badRequest().build();
         }
         int indexPos = participants.indexOf(participantRepository.getReferenceById(id));
         if(indexPos == -1){
-            throw new IllegalStateException("The participant with id " + id + " is not in this event.");
+            return ResponseEntity.badRequest().build();
         }
-        return participants.get(indexPos);
+        return ResponseEntity.ok(participants.get(indexPos));
 
     }
 
+    /**
+     * Post method that validates the participant to be added and adds it
+     * @param eventId the event the participant is added to
+     * @param participant the participant being added
+     * @return participant if successfully added to event
+     */
     public ResponseEntity<Participant> addParticipant(long eventId, Participant participant) {
-        List<Participant> currentParticipants = getAllParticipants(eventId);
+        List<Participant> currentParticipants = getAllParticipants(eventId).getBody();
 
         if(participant == null || participant.getName()==null ||
                 participant.getName().isEmpty()){
-            throw new IllegalStateException("Participant name should not be empty");
+            return ResponseEntity.badRequest().build();
         }
-        if(currentParticipants.contains(participant)){
-            throw new IllegalStateException("This participant is already a member of this event");
+        if(currentParticipants == null || currentParticipants.contains(participant)){
+            return ResponseEntity.badRequest().build();
         }
+
+        if(!validateBic(participant.getBic()) || !validateEmail(participant.getEmail())
+            || !validateIban(participant.getIban())){
+            return ResponseEntity.badRequest().build();
+        }
+
         currentParticipants.add(participant);
         Participant saved = participantRepository.save(participant);
         return ResponseEntity.ok(saved);
     }
 
+    /**
+     * Put method that validates the values in the participant to be changed
+     * @param eventId id of the event the participant is in
+     * @param id id of the participant to be retrieved
+     * @param name the new name to replace the participants name with
+     * @param email the new email to replace the participants email with
+     * @param iban the new iban to replace the participants iban with
+     * @param bic the new bic to replace the participants bic with
+     * @return participant if successfully added to event
+     */
     @Transactional
-    public ResponseEntity<Participant> updateParticipant(long eventId, long id, String name, String email, String iban, String bic) {
-        Participant participant = getParticipant(eventId,id);
-
+    public ResponseEntity<Participant> updateParticipant(long eventId, long id, String name,
+                                                         String email, String iban, String bic) {
+        Participant participant = getParticipant(eventId,id).getBody();
+        if(participant == null){
+            return ResponseEntity.badRequest().build();
+        }
         if(name != null && !name.isEmpty()
                 && !Objects.equals(participant.getName(), name)){
             participant.setName(name);
         }
         // TODO add more logic to update other fields
+        if(validateEmail(email)){
+            participant.setEmail(email);
+        }
+
+        if(validateIban(iban)){
+            participant.setIban(iban);
+        }
+
+        if(validateBic(bic)){
+            participant.setBic(bic);
+        }
+
         Participant saved = participantRepository.save(participant);
         return ResponseEntity.ok(saved);
     }
 
+    /**
+     * Delete method to remove a participant from the event
+     * @param eventId id of event participant is in
+     * @param id id of the participant to delete
+     */
     public void deleteParticipant(long eventId, long id) {
-        List<Participant> participants = getAllParticipants(eventId);
+        List<Participant> participants = getAllParticipants(eventId).getBody();
         boolean inRepo = participantRepository.existsById(id);
-        if(!inRepo){
+        if(!inRepo || participants == null){
             throw new IllegalStateException("Participant with id " + id +
                     " does not exist.");
         }
@@ -88,7 +146,38 @@ public class ParticipantService {
         if(!inEvent){
             throw new IllegalStateException("Participant with id " + id + " is not in this event.");
         }
-        int index = participants.indexOf(participantRepository.getReferenceById(id));
-        participants.remove(index);
+        participants.remove(participantRepository.getReferenceById(id));
+    }
+
+    /**
+     * Method to validate a String is in bic format
+     * @param bic the submitted bic string
+     * @return boolean value if the string is a valid bic number
+     */
+    private static boolean validateBic(String bic) {
+        String bicRegex = "^[A-Z]{4}[-]{0,1}[A-Z]{2}[-]{0,1}[A-Z0-9]{2}[-]{0,1}[0-9]{3}$";
+        return bic != null && Pattern.compile(bicRegex).matcher(bic).matches();
+    }
+
+    /**
+     * Method to validate a String is in iban format
+     * @param iban the submitted iban string
+     * @return boolean value if the string is a valid iban number
+     */
+    private static boolean validateIban(String iban) {
+        String ibanRegex = "^[A-Z]{2}[0-9]{2}(?:[ ]?[0-9]{4}){4}(?:[ ]?[0-9]{1,2})?$";
+        return iban != null && Pattern.compile(ibanRegex).matcher(iban).matches();
+    }
+
+    /**
+     * Method to validate a String is in email format
+     * @param email the submitted email string
+     * @return boolean value if the string is a valid email
+     */
+    private static boolean validateEmail(String email) {
+        String emailRegex = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+" +
+                "(\\\\.[A-Za-z0-9-]+)*(\\\\.[A-Za-z]{2,})$";
+        return email != null && Pattern.compile(emailRegex).matcher(email).matches();
+
     }
 }
