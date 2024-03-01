57f8ef83c99b3f4567df5949adc9bff2935c43e7
Philip Lekkerkerker <p.s.lekkerkerker@student.tudelft.nl>
2024-03-01 13:57:55 +0100

    Small rename to prevent merge conflict

--- server/src/main/java/server/api/TagController.java
+++ server/src/main/java/server/api/TagController.java
@@ -33,7 +33,7 @@ public class TagController {
      * @return returns a list of all events on the server
      */
     @GetMapping(path = { "/{inviteCode}/tags/{name}/expenses" })
-    public ResponseEntity<List<Expense>> getAll(@PathVariable("inviteCode") long inviteCode,
+    public ResponseEntity<List<Expense>> getAllExpensesWithTag(@PathVariable("inviteCode") long inviteCode,
                                                       @PathVariable("name") String tagName) {
         if (inviteCode < 0 || !repo.existsById(inviteCode) ||
                 repo.findById(inviteCode).get().getExpensesList() == null) {
