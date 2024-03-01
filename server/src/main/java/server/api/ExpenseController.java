d852eb6f164eb8c9507e6cbd6d68eb3e803f4c8d
Azra Ocak <a.ocak@student.tudelft.nl>
2024-03-02 00:14:35 +0100

    added put methods for payee, amount, and title of the expense

--- server/src/main/java/server/api/ExpenseController.java
+++ server/src/main/java/server/api/ExpenseController.java
@@ -4,10 +4,8 @@ import java.sql.Timestamp;
 import java.util.*;
 
 
-import commons.Expense;
-import commons.Event;
+import commons.*;
 
-import commons.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -41,7 +39,6 @@ public class ExpenseController {
         return ResponseEntity.ok(expenses);
     }
 
-
     /***
      * @param expense an expense to be added to the repo
      * @return code OK when the expense has been added successfully, Bad Request
@@ -49,20 +46,81 @@ public class ExpenseController {
      */
     @PostMapping(path = {"/api/events/{id}/expenses"})
     public ResponseEntity<Expense> add(@RequestBody Expense expense) {
-        if (expense == null || expense.getTitle() == null || Objects.equals(expense.getTitle(), "")) {
+        if (expense == null || expense.getTitle() == null ||
+                Objects.equals(expense.getTitle(), "") ||
+                        expense.getAmount() == 0 || expense.getPayee() == null) {
             return ResponseEntity.badRequest().build();
         }
         expenseRepo.save(expense);
         return ResponseEntity.ok(expense);
     }
 
+    /***
+     * @param title the desired new title
+     * @param expenseId the id of the expense to be updated
+     * @return whether the title of the expense could be updated
+     */
+    @PutMapping(path = {"/api/events/{eventId}/expenses/{expenseId}"})
+    public ResponseEntity<Void> changeTitle(@RequestBody String title,
+                                            @PathVariable("expenseId") long expenseId){
+        if (expenseId < 0 || !expenseRepo.existsById(expenseId)) {
+            return ResponseEntity.badRequest().build();
+        }
+        if (title == null || Objects.equals(title, "")) {
+            return ResponseEntity.badRequest().build();
+        }
+        Expense change = expenseRepo.findById(expenseId).get();
+        change.setTitle(title);
+        expenseRepo.save(change);
+        return ResponseEntity.ok(null);
+    }
+
+    /***
+     * @param amount the new amount the expense is worth
+     * @param expenseId the id of the expense to change the amount of
+     * @return whether the amount could be changed
+     */
+    @PutMapping(path = {"/api/events/{eventId}/expenses/{expenseId}"})
+    public ResponseEntity<Void> changeAmount(@RequestBody double amount,
+                                            @PathVariable("expenseId") long expenseId){
+        if (expenseId < 0 || !expenseRepo.existsById(expenseId)) {
+            return ResponseEntity.badRequest().build();
+        }
+        if (amount <= 0.0) {
+            return ResponseEntity.badRequest().build();
+        }
+        Expense change = expenseRepo.findById(expenseId).get();
+        change.setAmount(amount);
+        expenseRepo.save(change);
+        return ResponseEntity.ok(null);
+    }
+
+    /***
+     * @param payee the new payee of the event
+     * @param expenseId the ID of the expense to be updated
+     * @return whether the payee could be updated
+     */
+    @PutMapping(path = {"/api/events/{eventId}/expenses/{expenseId}"})
+    public ResponseEntity<Void> changePayee(@RequestBody Participant payee,
+                                             @PathVariable("expenseId") long expenseId){
+        if (expenseId < 0 || !expenseRepo.existsById(expenseId)) {
+            return ResponseEntity.badRequest().build();
+        }
+        if (payee == null || Objects.equals(payee.getName(), "")) {
+            return ResponseEntity.badRequest().build();
+        }
+        Expense change = expenseRepo.findById(expenseId).get();
+        change.setPayee(payee);
+        expenseRepo.save(change);
+        return ResponseEntity.ok(null);
+    }
 
     /***
      * @param expenseId ID of the expense to be deleted
-     * @return whether or not the expense could be deleted from the event
+     * @return whether the expense could be deleted from the event
      */
     @DeleteMapping(path = {"/api/events/{id}/expenses/{expenseId}"})
-    public ResponseEntity<Void> delete(@PathVariable("expenseId") long expenseId){
+    public ResponseEntity<Void> deleteExpense(@PathVariable("expenseId") long expenseId){
 
         if (expenseId < 0 || !expenseRepo.existsById(expenseId)) {
             return ResponseEntity.badRequest().build();
@@ -70,4 +128,5 @@ public class ExpenseController {
         expenseRepo.deleteAllById(Collections.singleton(expenseId));
         return ResponseEntity.ok(null);
     }
+
 }
