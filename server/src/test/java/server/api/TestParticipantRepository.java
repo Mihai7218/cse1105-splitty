b1124d74d0dd43b8651c26a538b68df42179fb9d
Stella Marlene Schultz <S.M.Schultz@student.tudelft.nl>
2024-03-02 09:52:37 +0100

    added javadoc so the pipeline passes

--- server/src/test/java/server/api/TestParticipantRepository.java
+++ server/src/test/java/server/api/TestParticipantRepository.java
@@ -22,34 +22,64 @@ public class TestParticipantRepository implements ParticipantRepository {
         calledMethods.add(name);
     }
 
+    /**
+     *
+     * @return
+     */
     @Override
     public List<Participant> findAll(){
         calledMethods.add("findAll");
         return participants;
     }
 
+    /**
+     *
+     * @param id must not be {@literal null}.
+     * @return
+     */
     @Override
     public Participant getReferenceById(Long id) {
         call("getReferenceById");
         return find(id).get();
     }
 
+    /**
+     *
+     * @param id must not be {@literal null}.
+     * @return
+     */
     @Override
     public Participant getById(Long id) {
         call("getById");
         return find(id).get();
     }
 
+    /**
+     *
+     * @param id
+     * @return
+     */
     private Optional<Participant> find(Long id) {
         return participants.stream().filter(q -> q.getId() == id).findFirst();
     }
 
+    /**
+     *
+     * @param id must not be {@literal null}.
+     * @return
+     */
     @Override
     public boolean existsById(Long id){
         call("existsById");
         return find(id).isPresent();
     }
 
+    /**
+     *
+     * @param entity must not be {@literal null}.
+     * @return
+     * @param <S>
+     */
     @Override
     public <S extends Participant> S save(S entity) {
         call("save");
@@ -58,6 +88,9 @@ public class TestParticipantRepository implements ParticipantRepository {
         return entity;
     }
 
+    /**
+     *
+     */
     @Override
     public void flush() {
         while(calledMethods.size()>0){
@@ -65,91 +98,192 @@ public class TestParticipantRepository implements ParticipantRepository {
         }
     }
 
+    /**
+     *
+     * @param entity entity to be saved. Must not be {@literal null}.
+     * @return
+     * @param <S>
+     */
     @Override
     public <S extends Participant> S saveAndFlush(S entity) {
         return null;
     }
 
+    /**
+     *
+     * @param entities entities to be saved. Must not be {@literal null}.
+     * @return
+     * @param <S>
+     */
     @Override
     public <S extends Participant> List<S> saveAllAndFlush(Iterable<S> entities) {
         return null;
     }
 
+    /**
+     *
+     * @param entities entities to be deleted. Must not be {@literal null}.
+     */
     @Override
     public void deleteAllInBatch(Iterable<Participant> entities) {
 
     }
 
+    /**
+     *
+     * @param longs the ids of the entities to be deleted. Must not be {@literal null}.
+     */
     @Override
     public void deleteAllByIdInBatch(Iterable<Long> longs) {
 
     }
 
+    /**
+     *
+     *
+     */
     @Override
     public void deleteAllInBatch() {
 
     }
 
+    /**
+     *
+     * @param aLong must not be {@literal null}.
+     * @return
+     */
     @Override
     public Participant getOne(Long aLong) {
         return null;
     }
 
+    /**
+     *
+     * @param example must not be {@literal null}.
+     * @return
+     * @param <S>
+     */
     @Override
     public <S extends Participant> Optional<S> findOne(Example<S> example) {
         return Optional.empty();
     }
 
+    /**
+     *
+     * @param example must not be {@literal null}.
+     * @return
+     * @param <S>
+     */
     @Override
     public <S extends Participant> List<S> findAll(Example<S> example) {
         return null;
     }
 
+    /**
+     *
+     * @param example must not be {@literal null}.
+     * @param sort the {@link Sort} specification to sort the results by, may be {@link Sort#unsorted()}, must not be
+     *          {@literal null}.
+     * @return
+     * @param <S>
+     */
     @Override
     public <S extends Participant> List<S> findAll(Example<S> example, Sort sort) {
         return null;
     }
 
+    /**
+     *
+     * @param example must not be {@literal null}.
+     * @param pageable the pageable to request a paged result, can be {@link Pageable#unpaged()}, must not be
+     *          {@literal null}.
+     * @return
+     * @param <S>
+     */
     @Override
     public <S extends Participant> Page<S> findAll(Example<S> example, Pageable pageable) {
         return null;
     }
 
+    /**
+     *
+     * @param example the {@link Example} to count instances for. Must not be {@literal null}.
+     * @return
+     * @param <S>
+     */
     @Override
     public <S extends Participant> long count(Example<S> example) {
         return 0;
     }
 
+    /**
+     *
+     * @param example the {@link Example} to use for the existence check. Must not be {@literal null}.
+     * @return
+     * @param <S>
+     */
     @Override
     public <S extends Participant> boolean exists(Example<S> example) {
         return false;
     }
 
+    /**
+     *
+     * @param example must not be {@literal null}.
+     * @param queryFunction the query function defining projection, sorting, and the result type
+     * @return
+     * @param <S>
+     * @param <R>
+     */
     @Override
     public <S extends Participant, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
         return null;
     }
 
+    /**
+     *
+     * @param entities must not be {@literal null} nor must it contain {@literal null}.
+     * @return
+     * @param <S>
+     */
     @Override
     public <S extends Participant> List<S> saveAll(Iterable<S> entities) {
         return null;
     }
 
+    /**
+     *
+     * @param aLong must not be {@literal null}.
+     * @return
+     */
     @Override
     public Optional<Participant> findById(Long aLong) {
         return Optional.empty();
     }
 
+    /**
+     *
+     * @param longs must not be {@literal null} nor contain any {@literal null} values.
+     * @return
+     */
     @Override
     public List<Participant> findAllById(Iterable<Long> longs) {
         return null;
     }
 
+    /**
+     *
+     * @return
+     */
     @Override
     public long count() {
         return 0;
     }
 
+    /**
+     *
+     * @param id must not be {@literal null}.
+     */
     @Override
     public void deleteById(Long id) {
         for(int i = 0; i<participants.size();i++){
@@ -161,31 +295,58 @@ public class TestParticipantRepository implements ParticipantRepository {
         }
     }
 
+    /**
+     *
+     * @param entity must not be {@literal null}.
+     */
     @Override
     public void delete(Participant entity) {
 
     }
 
+    /**
+     *
+     * @param longs must not be {@literal null}. Must not contain {@literal null} elements.
+     */
     @Override
     public void deleteAllById(Iterable<? extends Long> longs) {
 
     }
 
+    /**
+     *
+     * @param entities must not be {@literal null}. Must not contain {@literal null} elements.
+     */
     @Override
     public void deleteAll(Iterable<? extends Participant> entities) {
 
     }
 
+    /**
+     *
+     */
     @Override
     public void deleteAll() {
 
     }
 
+    /**
+     *
+     * @param sort the {@link Sort} specification to sort the results by, can be {@link Sort#unsorted()}, must not be
+     *          {@literal null}.
+     * @return
+     */
     @Override
     public List<Participant> findAll(Sort sort) {
         return null;
     }
 
+    /**
+     * 
+     * @param pageable the pageable to request a paged result, can be {@link Pageable#unpaged()}, must not be
+     *          {@literal null}.
+     * @return
+     */
     @Override
     public Page<Participant> findAll(Pageable pageable) {
         return null;
