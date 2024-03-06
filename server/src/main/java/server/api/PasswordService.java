a83f9e5a8fce0f0be976d0d2d2d46e5891fc1c0e
Philip Lekkerkerker <p.s.lekkerkerker@student.tudelft.nl>
2024-03-06 13:03:46 +0100

    Fixed Checkstyle

--- server/src/main/java/server/api/PasswordService.java
+++ server/src/main/java/server/api/PasswordService.java
@@ -3,13 +3,24 @@ package server.api;
 public class PasswordService {
     private static String password;
 
+    /**
+     * Empty Constructor for the passwordService
+     */
     public PasswordService() {
     }
 
+    /**
+     * Getter for the password
+     * @return Returns the current password
+     */
     public static String getPassword() {
         return password;
     }
 
+    /**
+     * setter for the password
+     * @param password The value to set the password to
+     */
     public static void setPassword(String password) {
         PasswordService.password = password;
     }
