afd462bd77ca65aa3cbdf94cb2e972ce27824df6
Mihai Bratu <m.a.bratu@student.tudelft.nl>
2024-03-08 00:41:39 +0100

    Add tests for LanguageComboBox

---
+++ client/src/test/java/client/utils/LanguageComboBoxTest.java
@@ -0,0 +1,27 @@
+package client.utils;
+
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(ApplicationExtension.class)
+class LanguageComboBoxTest {
+
+    LanguageComboBox sut;
+
+    @Start
+    void setUp(Stage stage) {
+        sut = new LanguageComboBox();
+    }
+    @Test
+    void testLanguageCodesNullList() {
+        assertEquals(List.of(), sut.getItems().stream().sorted().toList());
+    }
+
+}
