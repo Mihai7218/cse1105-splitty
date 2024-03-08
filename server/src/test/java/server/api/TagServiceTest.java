1846e342aff883a5fccffc42b436395de549965d
Azra Ocak <a.ocak@student.tudelft.nl>
2024-03-08 22:35:51 +0100

    working on TagServiceTest class

--- server/src/test/java/server/api/TagServiceTest.java
+++ server/src/test/java/server/api/TagServiceTest.java
@@ -1,4 +1,22 @@
 package server.api;
 
+import commons.Expense;
+import org.junit.jupiter.api.BeforeEach;
+import server.database.EventRepository;
+import server.database.TagRepository;
+
 public class TagServiceTest {
+    public TestEventRepository eventRepo;
+    public TestTagRepository tagRepo;
+    public TagService tagService;
+    public Expense expense1;
+    @BeforeEach
+    public void setup(){
+        eventRepo = new TestEventRepository();
+        tagRepo = new TestTagRepository();
+        tagService = new TagService(eventRepo, tagRepo);
+
+        //TODO: add payee and tag
+        expense1 = new Expense(500, "eur", "food", null, null, null, null, null);
+    }
 }
