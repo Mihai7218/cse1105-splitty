2d2cfc5e6ee68e94b6decb95157bc377f18e5d53
Azra Ocak <a.ocak@student.tudelft.nl>
2024-03-09 11:07:18 +0100

    added setup for tests

--- server/src/test/java/server/api/TagServiceTest.java
+++ server/src/test/java/server/api/TagServiceTest.java
@@ -1,7 +1,10 @@
 package server.api;
 
+import commons.Event;
 import commons.Expense;
+import commons.Tag;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import server.database.EventRepository;
 import server.database.TagRepository;
 
@@ -9,16 +12,26 @@ public class TagServiceTest {
     public TestEventRepository eventRepo;
     public TestTagRepository tagRepo;
     public TagService tagService;
-    public Expense expense1;
+    public Event event1;
+    public Event event2;
+    public Tag tag1;
+    public Tag tag2;
+
     @BeforeEach
-    public void setup(){
+    public void setup() {
         eventRepo = new TestEventRepository();
         tagRepo = new TestTagRepository();
         tagService = new TagService(eventRepo, tagRepo);
 
-        //TODO: add payee and tag
-        expense1 = new Expense(500, "eur", "food", null, null, null, null, null);
-    }
+        event1 = new Event("bowling", null, null);
+        event2 = new Event("picnic", null, null);
+        eventRepo.save(event1);
+        eventRepo.save(event2);
 
-    //TODO
+        tag1 = new Tag("food", "#ffffff");
+        tag2 = new Tag("drinks", "#ffd1dc");
+        tagRepo.save(tag1);
+        tagRepo.save(tag2);
+    }
+}
 }
