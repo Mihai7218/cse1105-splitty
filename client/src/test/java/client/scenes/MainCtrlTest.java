a6e8f1a279e9b19ca584ff4a475aa866ac1baefd
Vlad Cioculeasa <a.v.cioculeasa@student.tudelft.nl>
2024-03-17 22:42:01 +0100

    ctrlTest

--- client/src/test/java/client/scenes/MainCtrlTest.java
+++ client/src/test/java/client/scenes/MainCtrlTest.java
@@ -124,12 +124,17 @@ class MainCtrlTest {
         Parent participant = spy(Parent.class);
         OverviewCtrl overviewCtrl = mock(OverviewCtrl.class);
         Parent overview = spy(Parent.class);
+
+        AddExpenseCtrl addExpenseCtrl = mock(AddExpenseCtrl.class);
+        Parent addExpense = spy(Parent.class);
+
         sut.initialize(stage,
                 new Pair<>(quoteOverviewCtrl, quoteOverview),
                 new Pair<>(addQuoteCtrl, addQuote),
                 new Pair<>(startScreenCtrl, startScreen),
                 new Pair<>(participantCtrl, participant),
-                new Pair<>(overviewCtrl, overview));
+                new Pair<>(overviewCtrl, overview),
+                new Pair<>(addExpenseCtrl, addExpense));
         assertEquals(stage, sut.getPrimaryStage());
         assertEquals(quoteOverviewCtrl, sut.getQuoteOverviewCtrl());
         assertEquals(quoteOverview, sut.getQuoteOverview().getRoot());
