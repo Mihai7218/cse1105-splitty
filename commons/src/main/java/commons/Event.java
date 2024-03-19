6900fd4bbd0849ddaa07b91d2a3f7c06146ea239
Vlad Cioculeasa <a.v.cioculeasa@student.tudelft.nl>
2024-03-19 23:12:20 +0100

    implemented some add Methods

--- commons/src/main/java/commons/Event.java
+++ commons/src/main/java/commons/Event.java
@@ -83,6 +83,14 @@ public class Event {
     }
 
     /**
+     *
+     * @param expense to be added to the even
+     */
+    public void addExpense(Expense expense){
+        expensesList.add(expense);
+    }
+
+    /**
      * @param expensesList set the expenses list to a new list
      *                     TODO: addExpense method
      */
@@ -98,6 +106,14 @@ public class Event {
     }
 
     /**
+     *
+     * @param participant to be added to the event
+     */
+    public void addParticipant(Participant participant){
+        participantsList.add(participant);
+    }
+
+    /**
      * @param participantsList set the list of participants of the event
      *                         TODO: addParticipant method
      */
@@ -110,6 +126,14 @@ public class Event {
      */
     public List<Tag> getTagsList() {
         return tagsList;
+    }
+
+    /**
+     *
+     * @param tag to be added to the event
+     */
+    public void addTag(Tag tag){
+        tagsList.add(tag);
     }
 
     /**
