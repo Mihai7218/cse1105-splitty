dd3bbf8ab6ec5c17e790464f86ac4ea7ad626cdd
Zico Krassenburg <z.i.k.krassenburg@student.tudelft.nl>
2024-03-25 17:22:48 +0100

    new scene

--- server/src/main/java/server/api/ServerUtilModule.java
+++ server/src/main/java/server/api/ServerUtilModule.java
@@ -1,11 +1,13 @@
 package server.api;
 
 import commons.Event;
+import org.springframework.stereotype.Component;
 import server.database.EventRepository;
 
 import java.sql.Timestamp;
 import java.util.Date;
 
+@Component
 public class ServerUtilModule implements GerneralServerUtil{
 
     /**
