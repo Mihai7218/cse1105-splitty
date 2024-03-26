2b47a4fd516b0d2c5a78057a1dc3d499be7a328b
Stella Marlene Schultz <S.M.Schultz@student.tudelft.nl>
2024-03-26 17:06:54 +0100

    redoing the styling for all scenes

--- client/src/main/java/client/scenes/OverviewCtrl.java
+++ client/src/main/java/client/scenes/OverviewCtrl.java
@@ -113,7 +113,7 @@ public class OverviewCtrl implements Initializable {
         }
         addparticipant.setGraphic(new ImageView(new Image("icons/addParticipant.png")));
         settleDebts.setGraphic(new ImageView(new Image("icons/checkwhite.png")));
-        addExpenseButton.setGraphic(new ImageView(new Image("icons/whiteplus.png")));
+        addExpenseButton.setGraphic(new ImageView(new Image("icons/plus.png")));
         cancel.setGraphic(new ImageView(new Image("icons/cancelwhite.png")));
         Event event = mainCtrl.getEvent();
         clearFields();
