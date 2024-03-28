683a99668510dc9c5c8964637f1297b5d729a4ff
Mihai Bratu <m.a.bratu@student.tudelft.nl>
2024-03-28 23:50:30 +0200

    Fix server tests

--- server/src/test/java/server/api/ExpenseControllerTest.java
+++ server/src/test/java/server/api/ExpenseControllerTest.java
@@ -4,6 +4,7 @@ import commons.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import server.database.EventRepository;
 import server.database.ParticipantPaymentRepository;
 
@@ -11,9 +12,10 @@ import java.sql.Timestamp;
 import java.util.Date;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.springframework.http.HttpStatus.*;
 import static server.api.PasswordService.setPassword;
 
@@ -30,13 +32,15 @@ public class ExpenseControllerTest {
 
     public GerneralServerUtil serverUtil;
 
+    public SimpMessagingTemplate smt = mock(SimpMessagingTemplate.class);
+
     @BeforeEach
     public void setup() {
         serverUtil = new ServerUtilModule();
         TestExpenseRepository expenseRepo = new TestExpenseRepository();
         ParticipantPaymentRepository ppRepo = new TestParticipantPaymentRepository();
         ExpenseService serv = new ExpenseService(eventRepo, expenseRepo, ppRepo);
-        ctrl = new ExpenseController(serv,serverUtil, null);
+        ctrl = new ExpenseController(serv,serverUtil, smt);
         Date date = new Date();
         Timestamp timestamp2 = new Timestamp(date.getTime());
         event = new Event("main", timestamp2, timestamp2);
@@ -136,6 +140,7 @@ public class ExpenseControllerTest {
         Expense expense4 = new Expense(60, "party", "drinks",
                 null, null, null, null, payee);
         ctrl.add(eventId, expense4);
+        verify(smt).convertAndSend("/topic/events/0/expenses", expense4);
         //there should be 4 expenses in the event now
         assertEquals(4, ctrl.getAllExpenses(eventId).getBody().size());
     }
