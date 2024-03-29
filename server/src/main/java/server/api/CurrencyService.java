cb320330a4f43b9f1360b7bd6d19d81e39ad5f0f
Mihai Bratu <m.a.bratu@student.tudelft.nl>
2024-03-29 20:46:55 +0200

    Small fixes

--- server/src/main/java/server/api/CurrencyService.java
+++ server/src/main/java/server/api/CurrencyService.java
@@ -19,6 +19,14 @@ public class CurrencyService {
     private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
     private final ObjectMapper objectMapper = new ObjectMapper();
 
+    /**
+     * Method that gets the rate from the cached file or the fixer currency API
+     * @param date - date of the payment
+     * @param from - currency of the payment
+     * @param to - desired currency
+     * @return - 200 OK with the rate, 404 Not Found if currency codes are not found,
+     *           400 Bad Request if the date, from or to are not valid dates/currency codes
+     */
     public ResponseEntity<Double> getCurrency(String date, String from, String to) {
         if (from.length() != 3 && to.length() != 3)
             return ResponseEntity.badRequest().build();
@@ -27,8 +35,8 @@ public class CurrencyService {
         } catch (ParseException e) {
             return ResponseEntity.badRequest().build();
         }
-        File file = new File(String.valueOf(Path.of("server","src", "main", "resources", "rates", date, from, to + ".txt")));
-        System.out.println(file);
+        File file = new File(String.valueOf(Path.of("server", "src",
+                "main", "resources", "rates", date, from, to + ".txt")));
         try {
             if (file.equals(new File("null"))) throw new FileNotFoundException();
             return ResponseEntity.ok(new Scanner(file).nextDouble());
@@ -39,13 +47,16 @@ public class CurrencyService {
                         date, from, to)).toURL().openStream();
                 var obj = objectMapper.readTree(in);
                 double rate = obj.get("rates").get(to).asDouble();
-                new File(String.valueOf(Path.of("server", "src", "main", "resources", "rates", date, from))).mkdirs();
+                new File(String.valueOf(Path.of("server", "src",
+                        "main", "resources", "rates", date, from))).mkdirs();
                 PrintWriter pw = new PrintWriter(file);
                 pw.print(rate);
                 pw.flush();
                 return ResponseEntity.ok(rate);
             } catch (IOException | URISyntaxException ex) {
                 return ResponseEntity.internalServerError().build();
+            } catch (NullPointerException ex) {
+                return ResponseEntity.notFound().build();
             }
         } catch (NumberFormatException e) {
             return ResponseEntity.badRequest().build();
