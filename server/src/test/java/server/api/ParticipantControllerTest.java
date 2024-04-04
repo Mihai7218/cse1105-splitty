40bd07703b20047daf7465a03d7e609fae0b97ef
Stella Marlene Schultz <S.M.Schultz@student.tudelft.nl>
2024-04-04 09:30:05 +0200

    refactor participantControllerTest

--- server/src/test/java/server/api/ParticipantControllerTest.java
+++ server/src/test/java/server/api/ParticipantControllerTest.java
@@ -3,6 +3,9 @@ package server.api;
 import commons.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
+import server.database.EventRepository;
+import server.database.ParticipantRepository;
 
 
 import java.sql.Timestamp;
@@ -11,11 +14,112 @@ import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.*;
 import static server.api.PasswordService.setPassword;
 
 
 public class ParticipantControllerTest {
+
+    List<Participant> participantList;
+    static boolean validName;
+    static boolean validIban;
+    static boolean validBic;
+    static boolean validPayment;
+    static boolean validEmail;
+    public class ParticipantServiceStub extends ParticipantService {
+
+        /**
+         * ParticipantService Constructor
+         *
+         * @param eventRepository       the event repository to retrieve events from
+         * @param participantRepository
+         */
+        public ParticipantServiceStub(EventRepository eventRepository, ParticipantRepository participantRepository) {
+            super(eventRepository, participantRepository);
+            validName = false;
+            validIban = false;
+            validBic = false;
+            validEmail = false;
+            validPayment = false;
+            participantList = new ArrayList<>();
+
+        }
+
+        public ResponseEntity<List<Participant>> getAllParticipants(long eventId){
+            if(eventId < 0) return ResponseEntity.badRequest().build();
+            else if (eventId > 50) return ResponseEntity.notFound().build();
+            else return ResponseEntity.ok(participantList);
+        }
+
+        public ResponseEntity<Participant> getParticipant(long eventId, long id){
+            if(eventId < 0 || id < 0) return ResponseEntity.badRequest().build();
+            else if (eventId > 50 || id > 50)  return ResponseEntity.notFound().build();
+            else return ResponseEntity.ok(participantList.get((int) id));
+        }
+
+        public ResponseEntity<Participant> addParticipant(long eventId, Participant participant,
+                                                          GerneralServerUtil serverUtil){
+            if(eventId < 0) return ResponseEntity.badRequest().build();
+            else if (eventId > 50) return ResponseEntity.notFound().build();
+            else if(!validEmail || !validBic || !validName || !validIban){
+                return ResponseEntity.badRequest().build();
+            }
+            else{
+                participantList.add(participant);
+                serverUtil.updateDate(eventRepository,eventId);
+                return ResponseEntity.ok(participantList.getLast());
+            }
+        }
+
+        public ResponseEntity<Participant> updateParticipant(long eventId,
+                                                             long id, Participant participant,
+                                                             GerneralServerUtil serverUtil){
+            if(eventId < 0 || id < 0) return ResponseEntity.badRequest().build();
+            else if (eventId > 50 || id > 50) return ResponseEntity.notFound().build();
+            else if(!validEmail || !validBic || !validName || !validIban){
+                return ResponseEntity.badRequest().build();
+            }
+            else{
+                participantList.add(participant);
+                serverUtil.updateDate(eventRepository,eventId);
+                return ResponseEntity.ok(participantList.getLast());
+            }
+        }
+
+        public ResponseEntity<Participant> deleteParticipant(long eventId, long id,
+                                                             GerneralServerUtil serverUtil){
+            if(eventId < 0 || id < 0) return ResponseEntity.badRequest().build();
+            else if (eventId > 50 || id > 50) return ResponseEntity.notFound().build();
+            else{
+                return ResponseEntity.ok(participantList.remove((int)id));
+            }
+        }
+
+        public static boolean validateBic(String bic){
+            return validBic;
+        }
+
+        public static boolean validateBankInfo(Participant p){
+            return validPayment;
+        }
+
+        public static boolean validateName(String name){
+            return validName;
+        }
+
+        public static boolean validateIban(String iban){
+            return validIban;
+        }
+
+        public static boolean validateEmail(String email){
+            return validEmail;
+        }
+
+        public ResponseEntity<Participant> addPriorParticipant(Participant participant){
+            participantList.add(participant);
+            return ResponseEntity.ok(participant);
+        }
+    }
 
     public TestEventRepository eventRepository;
     public TestParticipantRepository participantRepository;
@@ -25,7 +129,7 @@ public class ParticipantControllerTest {
     public final Participant baseParticipant = new Participant("Chris Smith",
             "chrismsmith@gmail.com","NL85RABO5253446745",
             "HBUKGB4B");
-    public List<Participant> participantList;
+    public List<Participant> participantsList;
     public Date creationDate;
     public Date lastActivity;
 
@@ -33,6 +137,9 @@ public class ParticipantControllerTest {
     ParticipantService participantService;
 
     public GerneralServerUtil serverUtil;
+
+    ParticipantService serviceStubbed;
+    ParticipantController ctrlStubbed;
 
 
     @BeforeEach
@@ -50,121 +157,85 @@ public class ParticipantControllerTest {
                 "janedoe.com","NL85RABO5253446745",
                 "HBUKGB4B");
 
-        participantList = new ArrayList<>();
+        participantsList = new ArrayList<>();
         participantRepository.save(baseParticipant);
         participantRepository.save(valid);
-        participantList.add(baseParticipant);
-        participantList.add(valid);
+        participantsList.add(baseParticipant);
+        participantsList.add(valid);
         creationDate = new Date(124, 4, 20);
         lastActivity = new Date(124, 4, 25);
         Date date = new Date();
         Timestamp timestamp2 = new Timestamp(date.getTime());
         baseEvent = new Event("Mock Event",timestamp2,timestamp2);
         eventRepository.save(baseEvent);
-        eventRepository.getById(0L).setParticipantsList(participantList);
+        eventRepository.getById(0L).setParticipantsList(participantsList);
 
+        serviceStubbed = new ParticipantServiceStub(eventRepository, participantRepository);
+        ctrlStubbed = new ParticipantController(serviceStubbed, serverUtil);
 
     }
 
     @Test
     public void importParticipant(){
-        Event event = new Event("Title4", null, null);
         Participant p = new Participant("j doe", "example@email.com","NL85RABO5253446745", "HBUKGB4B");
-        Participant other = new Participant("John Doe",
-                "jdoe@gmail.com","NL85RABO5253446745",
-                "HBUKGB4B");
-        ParticipantPayment pp = new ParticipantPayment(other, 25);
-        List<ParticipantPayment> split = List.of(pp);
-        Tag t = new Tag("red", "red");
-        Expense e= new Expense(50, "USD", "exampleExpense", "description",
-                null,split ,t, p);
-        event.getParticipantsList().add(p);
-        event.getParticipantsList().add(other);
-        event.getExpensesList().add(e);
-        Tag one = new Tag("food", "#93c47d");
-        Tag two = new Tag("entrance fees", "#4a86e8");
-        Tag three = new Tag("travel", "#e06666");
-        event.setTagsList(List.of(t, one, two, three));
         setPassword("password");
-        event.setInviteCode(5);
-        assertEquals(OK, participantController.addPriorParticipant("password", p).getStatusCode());
+        assertEquals(ctrlStubbed.addPriorParticipant("password", p).getStatusCode(), OK);
+        assertEquals(ctrlStubbed.addPriorParticipant("wrongPassword", p).getStatusCode(), BAD_REQUEST);
     }
 
     @Test
     public void testGetController(){
-        participantRepository.flush();
-        eventRepository.flush();
-        participantController.getParticipants(0);
-        List<String> called = List.of("existsById", "findById","findById");
-        assertEquals(participantRepository.calledMethods.size(), 0);
-        assertEquals(eventRepository.calledMethods.size(), 3);
-        assertEquals(eventRepository.calledMethods, called);
+        assertEquals(OK, ctrlStubbed.getParticipants(0).getStatusCode());
+        assertEquals(BAD_REQUEST, ctrlStubbed.getParticipants(-1).getStatusCode());
+        assertEquals(NOT_FOUND, ctrlStubbed.getParticipants(100).getStatusCode());
     }
 
     @Test
     public void getAllControllerTest(){
-        participantRepository.flush();
-        eventRepository.flush();
-        participantController.getParticipant(0,0);
-        List<String> called = List.of("existsById", "findById","findById","existsById", "findById","findById");
-        assertEquals(eventRepository.calledMethods, called);
-        assertEquals(participantRepository.calledMethods.size(), 0);
-        assertEquals(eventRepository.calledMethods.size(), 6);
+        participantList.add(new Participant("test", null, null, null));
+        assertEquals(OK, ctrlStubbed.getParticipant(0,0).getStatusCode());
+        assertEquals(BAD_REQUEST, ctrlStubbed.getParticipant(-1,0).getStatusCode());
+        assertEquals(BAD_REQUEST, ctrlStubbed.getParticipant(0,-1).getStatusCode());
+        assertEquals(NOT_FOUND, ctrlStubbed.getParticipant(58,0).getStatusCode());
+        assertEquals(NOT_FOUND, ctrlStubbed.getParticipant(0,67).getStatusCode());
     }
 
     @Test
     public void addParticipantTest(){
-        participantRepository.flush();
-        eventRepository.flush();
-        Participant three = new Participant("Ethan", "eyoung@gmail.com",
-                "NL85RABO5253446745", "HBUKGB4B");
-        participantController.addParticipant(0, three);
-        List<String> called = List.of("existsById", "findById", "findById",
-                "existsById", "findById", "findById", "existsById",
-                "findById", "findById", "findById", "findById",
-                "save", "existsById", "getById", "save", "existsById", "getById");
-        assertEquals(eventRepository.calledMethods, called);
-        assertEquals(participantRepository.calledMethods.size(), 1);
-        assertEquals(eventRepository.calledMethods.size(), 17);
+        validBic = true;
+        validEmail = true;
+        validIban = true;
+        validPayment = true;
+        validName = true;
+        assertEquals(ctrlStubbed.addParticipant(0,new Participant("name", null, null,null)).getStatusCode(), OK);
+        assertEquals(ctrlStubbed.addParticipant(-1,new Participant("name", null, null,null)).getStatusCode(), BAD_REQUEST);
+        assertEquals(ctrlStubbed.addParticipant(51,new Participant("name", null, null,null)).getStatusCode(), NOT_FOUND);
     }
 
     @Test
     public void updateParticipantTest(){
-        participantRepository.flush();
-        eventRepository.flush();
-        Participant p = new Participant("Christina Smith", "cmsmith@yahoo.com",
-                "NL85ABNA5253446745", "AMUKGB7B");
-        participantController.updateParticipant(0, 0, p);
-        List<String> called = List.of("existsById", "findById","findById",
-                "existsById", "findById","findById",
-                "existsById", "findById","findById",
-                "existsById", "findById","findById",
-                "existsById", "findById","findById",
-                "existsById", "findById","findById",
-                "findById", "save", "existsById",
-                "getById");
-        assertEquals(eventRepository.calledMethods, called);
-        assertEquals(participantRepository.participants.get(0).getName(), "Christina Smith");
-        assertEquals(participantRepository.participants.size(), 2);
-        assertEquals(eventRepository.calledMethods.size(), 22);
+        validBic = true;
+        validEmail = true;
+        validIban = true;
+        validPayment = true;
+        validName = true;
+        assertEquals(ctrlStubbed.updateParticipant(0,0,new Participant("test", null, null, null)).getStatusCode(), OK);
+        assertEquals(ctrlStubbed.updateParticipant(0,-1,new Participant("test", null, null, null)).getStatusCode(), BAD_REQUEST);
+        assertEquals(ctrlStubbed.updateParticipant(-1,0,new Participant("test", null, null, null)).getStatusCode(), BAD_REQUEST);
+        assertEquals(ctrlStubbed.updateParticipant(100,0,new Participant("test", null, null, null)).getStatusCode(), NOT_FOUND);
+        assertEquals(ctrlStubbed.updateParticipant(0,100,new Participant("test", null, null, null)).getStatusCode(), NOT_FOUND);
+        validName = false;
+        assertEquals(ctrlStubbed.updateParticipant(0,0, new Participant("", null, null, null)).getStatusCode(), BAD_REQUEST);
     }
 
     @Test
     public void deleteParticipantsTest(){
-        participantRepository.flush();
-        eventRepository.flush();
-        participantController.deleteParticipant(0,0);
-        assertEquals(participantRepository.calledMethods.size(), 1);
-        assertEquals(eventRepository.calledMethods.size(), 27);
-        List<String> called = List.of("existsById", "findById", "findById",
-                "existsById", "findById", "findById", "existsById",
-                "findById", "findById", "existsById", "findById",
-                "findById", "existsById", "findById", "findById",
-                "existsById", "findById", "findById", "findById",
-                "findById", "save", "existsById", "getById", "save",
-                "existsById", "getById", "getReferenceById");
-        assertEquals(eventRepository.calledMethods, called);
-
+        participantList.add(new Participant("test", null,null,null));
+        assertEquals(ctrlStubbed.deleteParticipant(0,0).getStatusCode(), OK);
+        assertEquals(ctrlStubbed.deleteParticipant(-1,0).getStatusCode(), BAD_REQUEST);
+        assertEquals(ctrlStubbed.deleteParticipant(0,-1).getStatusCode(), BAD_REQUEST);
+        assertEquals(ctrlStubbed.deleteParticipant(100,0).getStatusCode(), NOT_FOUND);
+        assertEquals(ctrlStubbed.deleteParticipant(0,100).getStatusCode(), NOT_FOUND);
     }
 
 
