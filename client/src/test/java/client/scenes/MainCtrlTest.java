0779df4788b0770f43fa03776e3a6e439aa035c5
Azra Ocak <a.ocak@student.tudelft.nl>
2024-04-12 22:49:58 +0200

    wrote tests for the confirmations and other methods

--- client/src/test/java/client/scenes/MainCtrlTest.java
+++ client/src/test/java/client/scenes/MainCtrlTest.java
@@ -1,5 +1,6 @@
 package client.scenes;
 
+import client.commands.ICommand;
 import client.utils.LanguageManager;
 import com.sun.javafx.collections.ObservableListWrapper;
 import commons.Event;
@@ -21,8 +22,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Stack;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(ApplicationExtension.class)
@@ -79,10 +79,6 @@ class MainCtrlTest {
         verify(stage).setScene(any());
     }
 
-    @Test //TODO; verify behaviour for the stylesheets
-    void showOverview2() {
-
-    }
 
     /**
      * Tests that the title of the stage was bound to "startScreen.windowTitle",
@@ -149,9 +145,21 @@ class MainCtrlTest {
 
     }
     //TODO: showAddExpense, showConnectToServer, showExpenseConfirmation, showParticipantConfirmation
-    //TODO: showExpenseConfirmation, showParticipantConfirmation
-    //TODO: showInviteConfirmation, showEditConfirmation
     //TODO: pickLocation, getSettingsCtrl, getSettings,
+
+
+    @Test
+    void showEditTransfer(){
+        sut.showEditTransfer();
+        assertEquals(null, stageTitle);
+        verify(stage).setScene(any());
+    }
+    @Test
+    void showTransfer(){
+        sut.showTransfer();
+        assertEquals(null, stageTitle);
+        verify(stage).setScene(any());
+    }
 
     @Test
     void showInvitation(){
@@ -188,7 +196,121 @@ class MainCtrlTest {
         verify(stage).setScene(any());
     }
 
+    @Test
+    void showConfirmations(){
+        //Mocks of all the controllers
+        StartScreenCtrl startScreenCtrl = mock(StartScreenCtrl.class);
+        Parent startScreen = spy(Parent.class);
+        ParticipantCtrl participantCtrl = mock(ParticipantCtrl.class);
+        Parent participant = spy(Parent.class);
+        OverviewCtrl overviewCtrl = mock(OverviewCtrl.class);
+        Parent overview = spy(Parent.class);
+        AddExpenseCtrl addExpenseCtrl = mock(AddExpenseCtrl.class);
+        Parent addExpense = spy(Parent.class);
+        InvitationCtrl invitationCtrl = mock(InvitationCtrl.class);
+        Parent invitation = spy(Parent.class);
+        EditParticipantCtrl editParticipantCtrl = mock(EditParticipantCtrl.class);
+        Parent editParticipant = spy(Parent.class);
+        SettingsCtrl settingsCtrl = mock(SettingsCtrl.class);
+        Parent settings = spy(Parent.class);
+        StatisticsCtrl statisticsCtrl = mock(StatisticsCtrl.class);
+        Parent statistics = spy(Parent.class);
+        EditExpenseCtrl editExpenseCtrl = mock(EditExpenseCtrl.class);
+        Parent editExpense = spy(Parent.class);
+        ConnectToServerCtrl connectToServerCtrl = mock(ConnectToServerCtrl.class);
+        Parent connectToServer = spy(Parent.class);
+        DebtsCtrl debtsCtrl = mock(DebtsCtrl.class);
+        Parent debts = spy(Parent.class);
+        ManageTagsCtrl manageTagsCtrl = mock(ManageTagsCtrl.class);
+        Parent manageTags = spy(Parent.class);
+        EditTagCtrl editTagCtrl = mock(EditTagCtrl.class);
+        Parent editTag = spy(Parent.class);
+        Parent transfer = spy(Parent.class);
+        AddTransferCtrl transferCtrl = mock(AddTransferCtrl.class);
+        EditTransferCtrl editTransferCtrl = mock(EditTransferCtrl.class);
+        Parent editTransfer = spy(Parent.class);
+        sut.initialize(stage,
+                new Pair<>(startScreenCtrl, startScreen),
+                new Pair<>(participantCtrl, participant),
+                new Pair<>(overviewCtrl, overview),
+                new Pair<>(addExpenseCtrl, addExpense),
+                new Pair<>(invitationCtrl,invitation),
+                new Pair<>(editParticipantCtrl,editParticipant),
+                new Pair<>(settingsCtrl, settings),
+                new Pair<>(statisticsCtrl,statistics),
+                new Pair<>(editExpenseCtrl, editExpense),
+                new Pair<>(connectToServerCtrl, connectToServer),
+                new Pair<>(debtsCtrl, debts),
+                new Pair<>(manageTagsCtrl, manageTags),
+                new Pair<>(editTagCtrl, editTag),
+                new Pair<>(transferCtrl, transfer),
+                new Pair<>(editTransferCtrl,editTransfer));
+        sut.showExpenseConfirmation();
+        verify(overviewCtrl).showNotification("overview.confirmExpenseAdd");
+        sut.showInviteConfirmation();
+        verify(overviewCtrl).showNotification("overview.confirmInvite");
+        sut.showEditConfirmation();
+        verify(overviewCtrl).showNotification("overview.confirmEdits");
+        sut.showParticipantConfirmation();
+        verify(overviewCtrl).showNotification("overview.confirmParticipantAdd");
+    }
 
+    @Test
+    void gettersAndSetters(){
+        //Mocks of all the controllers
+        StartScreenCtrl startScreenCtrl = mock(StartScreenCtrl.class);
+        Parent startScreen = spy(Parent.class);
+        ParticipantCtrl participantCtrl = mock(ParticipantCtrl.class);
+        Parent participant = spy(Parent.class);
+        OverviewCtrl overviewCtrl = mock(OverviewCtrl.class);
+        Parent overview = spy(Parent.class);
+        AddExpenseCtrl addExpenseCtrl = mock(AddExpenseCtrl.class);
+        Parent addExpense = spy(Parent.class);
+        InvitationCtrl invitationCtrl = mock(InvitationCtrl.class);
+        Parent invitation = spy(Parent.class);
+        EditParticipantCtrl editParticipantCtrl = mock(EditParticipantCtrl.class);
+        Parent editParticipant = spy(Parent.class);
+        SettingsCtrl settingsCtrl = mock(SettingsCtrl.class);
+        Parent settings = spy(Parent.class);
+        StatisticsCtrl statisticsCtrl = mock(StatisticsCtrl.class);
+        Parent statistics = spy(Parent.class);
+        EditExpenseCtrl editExpenseCtrl = mock(EditExpenseCtrl.class);
+        Parent editExpense = spy(Parent.class);
+        ConnectToServerCtrl connectToServerCtrl = mock(ConnectToServerCtrl.class);
+        Parent connectToServer = spy(Parent.class);
+        DebtsCtrl debtsCtrl = mock(DebtsCtrl.class);
+        Parent debts = spy(Parent.class);
+        ManageTagsCtrl manageTagsCtrl = mock(ManageTagsCtrl.class);
+        Parent manageTags = spy(Parent.class);
+        EditTagCtrl editTagCtrl = mock(EditTagCtrl.class);
+        Parent editTag = spy(Parent.class);
+        Parent transfer = spy(Parent.class);
+        AddTransferCtrl transferCtrl = mock(AddTransferCtrl.class);
+        EditTransferCtrl editTransferCtrl = mock(EditTransferCtrl.class);
+        Parent editTransfer = spy(Parent.class);
+        sut.initialize(stage,
+                new Pair<>(startScreenCtrl, startScreen),
+                new Pair<>(participantCtrl, participant),
+                new Pair<>(overviewCtrl, overview),
+                new Pair<>(addExpenseCtrl, addExpense),
+                new Pair<>(invitationCtrl,invitation),
+                new Pair<>(editParticipantCtrl,editParticipant),
+                new Pair<>(settingsCtrl, settings),
+                new Pair<>(statisticsCtrl,statistics),
+                new Pair<>(editExpenseCtrl, editExpense),
+                new Pair<>(connectToServerCtrl, connectToServer),
+                new Pair<>(debtsCtrl, debts),
+                new Pair<>(manageTagsCtrl, manageTags),
+                new Pair<>(editTagCtrl, editTag),
+                new Pair<>(transferCtrl, transfer),
+                new Pair<>(editTransferCtrl,editTransfer));
+        sut.addToHistory(mock(ICommand.class));
+        assertNotNull(sut.getHistory());
+        assertEquals(sut.getSettingsCtrl(), settingsCtrl);
+        assertEquals(sut.getTagCtrl(), editTagCtrl);
+        assertEquals(sut.getEditTransferCtrl(), editTransferCtrl);
+        assertEquals(sut.getTransferCtrl(), transferCtrl);
+    }
     /**
      * Tests that the stage that was set is returned when calling the getter for it.
      */
